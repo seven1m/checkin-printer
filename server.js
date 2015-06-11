382caa21099d25d53e2837d91fa7e605bcbd20a4
Tim Morgan <tim@timmorgan.org>
2015-06-10 23:09:31 -0700

    Start work on v2, now using Pusher

--- server.js
+++ server.js
@@ -1,40 +1,33 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var spawn = require('child_process').spawn;
+/*
+TODO:
+1. Grab labels via API from OneBody
+2. Copy over Checkin.LabelSet code from OneBody
+3. Connect via CUPS to Dymo to print
+*/
 
-var server = http.createServer(function (req, res) {
-  var q, query = url.parse(req.url, true).query;
-
-  fs.readFile(__dirname + '/label.pdf', {encoding: 'utf8'}, function(err, pdf) {
-    pdf = pdf.toString();
-    for(q in query) {
-      var val = query[q].replace(/([^a-z0-9 \-_'"])/ig, "\\$1")
-      pdf = pdf.replace(new RegExp("<" + q + ">", 'g'), val);
+var Pusher = require('pusher-client');
+var pusher = new Pusher(
+    process.env.PUSHER_API_KEY,
+    {
+        secret: process.env.PUSHER_API_SECRET,
+        channel_data: {
+            user_id: process.env.PRINTER_ID,
+            user_info: {
+                name: 'checkin-printer-node'
+            }
+        }
     }
+);
 
-    var lpr = spawn('lpr');
-    lpr.stdin.end(pdf);
+var prints = pusher.subscribe('private-prints-' + process.env.PRINTER_ID);
 
-    var out = '';
-    lpr.stdout.on('data', function(data) { out += data })
-    lpr.stderr.on('data', function(data) { out += data })
+var presence = pusher.subscribe('presence-prints');
 
-    lpr.on('exit', function(code) {
-      if(code == 0) {
-        console.log('print success:', query);
-        res.writeHead(200);
-        res.end();
-      } else {
-        console.log('print fail:', query);
-        console.log(out);
-        res.writeHead(500);
-        res.end(out)
-      }
-    });
-  });
+prints.bind('print', function(data) {
+    console.log('print');
+    console.log(data);
 })
 
-server.listen(8080);
-
-console.log('Server running on port 8080');
+pusher.connection.bind('state_change', function(state) {
+    console.log(state.current);
+});
